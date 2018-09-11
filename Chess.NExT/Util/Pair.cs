30f5eb21c76a21b3efbc8c56e340bc735fa9e7b3
Adam J. Leuer <aleuer2@illinois.edu>
2018-09-10 23:18:32 -0500

    Changed namespace of Pair to match project

--- Chess.NExT/Util/Pair.cs
+++ Chess.NExT/Util/Pair.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 
-namespace Elevator.NET.Utility
+namespace Chess.Util
 {
     public class Pair<T> : IList<T>, ITuple
     {
